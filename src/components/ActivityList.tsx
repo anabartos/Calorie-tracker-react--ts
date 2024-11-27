a1dbcad5bc2f1d30c201da25f99629b8817c189d
Ana Bartos <ana.l.bartos@gmail.com>
2024-11-27 12:53:31 +0100

    cambios estilos

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,55 +1,56 @@
-import { useMemo, Dispatch} from "react"
+import { useMemo, Dispatch } from "react"
 import { Activity } from "../types"
 import { categories } from "../data/categories"
 import { PencilSquareIcon, XCircleIcon, } from "@heroicons/react/24/outline"
 import { ActivityActions } from "../reducer/activityReducer"
 
 type ActivitiListProps = {
-    activities: Activity[],
-    dispatch:Dispatch<ActivityActions>
+  activities: Activity[],
+  dispatch: Dispatch<ActivityActions>
 }
 
-export default function ActivityList({activities, dispatch} : ActivitiListProps) {
+export default function ActivityList({ activities, dispatch }: ActivitiListProps) {
 
-  const categoryName = useMemo(() => 
-    (category: Activity["category"]) => categories.map( cat => cat.id === category ? cat.name : "") 
-  , [activities])
+  const categoryName = useMemo(() =>
+    (category: Activity["category"]) => categories.map(cat => cat.id === category ? cat.name : "")
+    , [activities])
 
   const isEmptyActivities = useMemo(() => activities.length === 0, [activities])
   return (
     <>
-        <h2 className="text-3xl font-bold text-center">Food and Activities</h2>
-        {isEmptyActivities ? 
-          <p className="text-center my-5">No activies yet</p> : 
-          activities.map(activity => (
-            <div key={activity.id}
-                 className="px-5 py-10 bg-white mt-5 flex justify-between shadow-sm">
-                 <div className="space-y-2 relative">
-                    <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${activity.category === 1 ? "bg-sky-950" : "bg-fuchsia-900"}`}>
-                      {categoryName(+activity.category)}
-                    </p>
-                    <p className="text-2xl font-bold pt-5">{activity.name}</p>
-                    <p className="font-black text-3xl text-fuchsia-500">
-                        {activity.calories}{""} <span>Calories</span>
-                    </p>
-                 </div>
-                 <div className="flex-gap-5 items-center">
-                    <button
-                      onClick={() => dispatch({ type: "set-activeId", payload: {id: activity.id}})}
-                    >
-                      <PencilSquareIcon
-                      className="h-8 w-8 text-sky-950"
-                      />
-                    </button>
-                    <button
-                      onClick={() => dispatch({ type: "delete-activity", payload: {id: activity.id}})}
-                    >
-                      <XCircleIcon
-                      className="h-8 w-8 text-fuchsia-800"
-                      />
-                    </button>
-                 </div>
+      <h2 className="text-3xl font-bold text-stone-800 text-center">FOOD & ACTIVITIES</h2>
+      {isEmptyActivities ?
+        <p className=" text-stone-700 text-center my-5">No activies yet</p> :
+        activities.map(activity => (
+          <div key={activity.id}
+            className="px-5 py-10  mt-5 flex justify-between shadow-sm">
+            <div className="space-y-2 relative">
+              <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${activity.category === 1 ? "bg-rose-400" : "bg-yellow-600"}`}>
+                {categoryName(+activity.category)}
+              </p>
+              <p className="text-2xl text-stone-700 font-bold pt-5">{activity.name}</p>
+              <p className="font-black  text-stone-700 text-3xl">
+                {activity.calories}{""} <span>Calories</span>
+              </p>
             </div>
+            <div className="flex-gap-5 items-center">
+              <button
+                onClick={() => dispatch({ type: "set-activeId", payload: { id: activity.id } })}
+              >
+                <PencilSquareIcon
+                  className="h-8 w-8 text-stone-700"
+                />
+              </button>
+              <button
+                onClick={() => dispatch({ type: "delete-activity", payload: { id: activity.id } })}
+              >
+                <XCircleIcon
+                  className="h-8 w-8 text-rose-400
+                      "
+                />
+              </button>
+            </div>
+          </div>
         ))}
     </>
   )
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -16,95 +16,89 @@ const initialState : Activity = {
   calories:0
 }
 
-export default function Form({dispatch, state}: FormProps) {
-    
-    const [activity, setActivity] = useState<Activity>(initialState)
+export default function Form({ dispatch, state }: FormProps) {
 
-    useEffect(() => {
-      if(state.activeId){
-        const selectedActivity = state.activities.filter( stateActivity => stateActivity.id === state.activeId)[0]
+  const [activity, setActivity] = useState<Activity>(initialState)
+
+  useEffect(() => {
+    if (state.activeId) {
+      const selectedActivity = state.activities.filter(stateActivity => stateActivity.id === state.activeId)[0]
       setActivity(selectedActivity)
-      }
-    }, [state.activeId])
-
-
-    const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
-      const isNumberField = ["category", "calories"].includes(e.target.id)
-      setActivity({
-        ...activity,
-        [e.target.id]: isNumberField ? +e.target.value : e.target.value
-      })
     }
+  }, [state.activeId])
 
-    const isValidActivity = () => {
-        const {name, calories} = activity
-        return name.trim() !== "" && calories > 0
-    }
+
+  const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
+    const isNumberField = ["category", "calories"].includes(e.target.id)
+    setActivity({
+      ...activity,
+      [e.target.id]: isNumberField ? +e.target.value : e.target.value
+    })
+  }
+
+  const isValidActivity = () => {
+    const { name, calories } = activity
+    return name.trim() !== "" && calories > 0
+  }
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-      e.preventDefault()
-      dispatch({ type: "save-activity", payload: {newActivity: activity}})
+    e.preventDefault()
+    dispatch({ type: "save-activity", payload: { newActivity: activity } })
 
-      setActivity({
-        ...initialState,
-        id: uuidv4()
-      })
+    setActivity({
+      ...initialState,
+      id: uuidv4()
+    })
   }
 
   return (
     <div>
-      <form 
-        className="space-y-5 bg-slate-100 shadow p-10 rounded-lg"
+      <form
+        className="space-y-8 p-10 rounded-lg"
         onSubmit={handleSubmit}
-        >
+      >
         <div className="grid grid-cols-1 gap-3">
-            <label htmlFor="category" className="font-bold">Category:</label>
-            <select 
-              className="border border-slate-300 p-2 rounded-lg w-full bg-white"
-              id="category"
-              value={activity.category}
-              onChange={handleChange}
-                >
-                 {categories.map(category => (
-                    <option
-                    key={category.id}
-                    value={category.id}>
-                      
-                        {category.name}
-                    </option>
-                 ))}
-                </select>
+          <label htmlFor="category" className="font-extrabold">CATEGORY:</label>
+          <select
+            className="p-2 rounded-lg w-full"
+            id="category"
+            value={activity.category}
+            onChange={handleChange}
+          >
+            {categories.map(category => (
+              <option
+                key={category.id}
+                value={category.id}>
+                {category.name}
+              </option>
+            ))}
+          </select>
         </div>
-
         <div className="grid grid-cols-1 gap-3">
-            <label htmlFor="name" className="font-bold">Activity:</label>
-            <input id="name"
-                   type="text" 
-                   className="border border-slate-300 p-2 rounded-lg"
-                   placeholder="ej. Food, juice, salad, exersice, bike "
-                   value={activity.name}
-                   onChange={handleChange}
-
-            />
+          <label htmlFor="name" className="font-extrabold">ACTIVITY:</label>
+          <input id="name"
+            type="text"
+            className="border border-slate-300 p-2 rounded-lg"
+            placeholder="ej. Food, juice, salad, exersice, bike "
+            value={activity.name}
+            onChange={handleChange}
+          />
         </div>
-
         <div className="grid grid-cols-1 gap-3">
-            <label htmlFor="calories" className="font-bold">Calories:</label>
-            <input id="calories"
-                   type="text" 
-                   className="border border-slate-300 p-2 rounded-lg"
-                   placeholder="Calories. ej. 300 o 500"
-                   value={activity.calories}
-                   onChange={handleChange}
-            />
+          <label htmlFor="calories" className="font-extrabold">CALORIES:</label>
+          <input id="calories"
+            type="text"
+            className="border border-slate-300 p-2 rounded-lg"
+            placeholder="Calories. ej. 300 o 500"
+            value={activity.calories}
+            onChange={handleChange}
+          />
         </div>
-
-        <input 
-            type="submit"
-            className="mx-auto block w-full sm:w-64 bg-sky-800 hover:bg-gray-900 p-2 font-bold uppercase text-white rounded-md cursor-pointer disabled:opacity-10" 
-            value={activity.category === 1 ? "Save Food" : "Save Exercise"}
-            disabled={!isValidActivity()}/>
-
+        <input
+          type="submit"
+          className="mx-auto block w-full sm:w-64 bg-yellow-600 hover:bg-stone-800 p-2 font-bold uppercase text-white rounded-md cursor-pointer disabled:opacity-10"
+          value={activity.category === 1 ? "Save Food" : "Save Exercise"}
+          disabled={!isValidActivity()} />
       </form>
     </div>
   )
